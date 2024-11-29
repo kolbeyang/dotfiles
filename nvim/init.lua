ee9f2b4f7a1a319e7092f8cd16de46dc77df754e
Kolbe Yang <kolbe@glassdomeinc.com>
2024-11-29 23:05:42 +0900

    feat: snippets

--- nvim/init.lua
+++ nvim/init.lua
@@ -644,6 +644,11 @@ require('lazy').setup({
           end
           return 'make install_jsregexp'
         end)(),
+        config = function()
+          require('luasnip.loaders.from_vscode').lazy_load {
+            paths = { '~/.config/nvim/snippets' }, -- Custom path for your snippets
+          }
+        end,
         dependencies = {
           -- `friendly-snippets` contains a variety of premade snippets.
           -- See the README about individual language/framework/plugin snippets:
--- nvim/lua/plugins/nvim-scissors.lua
+++ nvim/lua/plugins/nvim-scissors.lua
@@ -2,6 +2,15 @@ return {
   'chrisgrieser/nvim-scissors',
   dependencies = 'nvim-telescope/telescope.nvim',
   opts = {
-    snippetDir = '../snippets/',
+    snippetDir = '../../snippets/',
   },
+  config = function()
+    vim.keymap.set('n', '<leader>je', function()
+      require('scissors').editSnippet()
+    end)
+    -- when used in visual mode, prefills the selection as snippet body
+    vim.keymap.set({ 'n', 'x' }, '<leader>ja', function()
+      require('scissors').addNewSnippet()
+    end)
+  end,
 }
